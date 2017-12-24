fad57be4fe85289cab7a2febe90c3649f4e62b6b
dperez <dperez@Mint-F555L>
2017-12-24 17:23:12 +0100

    .-.-

--- CarCube/src/atunibz/dcube/DBProject/GUI/ContactPanel.java
+++ CarCube/src/atunibz/dcube/DBProject/GUI/ContactPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class ContactPanel extends BackgroundedPanel {
 	
-	private JLabel nameLbl, surnameLbl, taxLbl, phoneLbl, mailLbl, faxLbl;
+	private IconLabel nameLbl, surnameLbl, taxLbl, phoneLbl, mailLbl, faxLbl;
 	private JTextField nameTF, surnameTF, taxTF, phoneTF, mailTF, faxTF;
 	private JButton backBtn, statsBtn;
 	
@@ -14,17 +14,23 @@ public class ContactPanel extends BackgroundedPanel {
 	
 	private void initComponents() {
 		//name
-		nameLbl = new JLabel("Name:");
+		nameLbl = new IconLabel(null,"Name:", false);
 		nameTF = new JTextField(10);
 		//surname
-		surnameLbl = new JLabel("Surname:");
+		surnameLbl = new IconLabel(null, "Surname:", false);
 		surnameTF = new JTextField(15);
 		//taxcode
-		taxLbl = new JLabel("Taxcode:");
+		taxLbl = new IconLabel(null,"Taxcode:", false);
 		taxTF = new JTextField(16);
 		//phone
-		phoneLbl = new JLabel("Phone:");
+		phoneLbl = new IconLabel(null,"Phone:", false);
 		phoneTF = new JTextField(15);
+		//mail
+		mailLbl = new IconLabel(null,"Mail:", false);
+		mailTF = new JTextField(20);
+		//fax
+		faxLbl = new IconLabel(null,"Fax:", false);
+		faxTF = new JTextField(15);
 		
 		
 	}
