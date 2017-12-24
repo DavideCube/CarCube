02ec689461740d7ed4597121538f9e9a9ab5097b
dperez <dperez@Mint-F555L>
2017-12-24 17:02:12 +0100

    test class

---
+++ CarCube/src/atunibz/dcube/DBProject/GUI/Test.java
@@ -0,0 +1,21 @@
+package atunibz.dcube.DBProject.GUI;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame ("CarCube");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(1435, 800));
+		frame.getContentPane().add(new StakeholdersPanel());
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+
+	}
+
+}
