41856ba5ac6bc1b87bbfd8dbca6696ba6f80f2af
CremoD <cremo.davide96@gmail.com>
2018-01-18 14:53:31 +0100

    test per perz

--- CarCube/src/atunibz/dcube/DBProject/GUI/Test.java
+++ CarCube/src/atunibz/dcube/DBProject/GUI/Test.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 public class Test {
 
 	public static void main(String[] args) {
-			JPanel p = new SalesLineChart();
+			JPanel p = new CarPieChart();
 			JFrame f = new JFrame();
 			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			f.getContentPane().add(p);
